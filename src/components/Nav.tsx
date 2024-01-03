278dc87acefcf3b2e894d720cbd2b27c22cdfb22
winnerx <deusdamicrosoft@gmail.com>
2024-01-02 21:26:07 -0300

    feat: Commit

---
+++ src/components/Nav.tsx
@@ -0,0 +1,8 @@
+export const Nav = () => {
+  return <> 
+      <a className="text-[#38F2AF] font-medium text-lg " href="http://">Dashboard</a>  
+      <a className="text-[#38F2AF] font-medium text-lg " href="http://">Dashboard</a>  
+      <a className="text-[#38F2AF] font-medium text-lg " href="http://">Dashboard</a>  
+  </>
+
+}
